67f26ffa3b10d727357a03ad044bb5b9a7bdeb79
anitha-anii <anithachowdary355@gmail.com>
2023-07-11 09:11:50 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,52 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import "../styles/App.css";
 
+
+const App = () => {
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+
+  const clear = () => {
+    setResult('');
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={result} readOnly />
+      </div>
+      <div className="buttons">
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="+" onClick={handleClick}>+</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="-" onClick={handleClick}>-</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="*" onClick={handleClick}>*</button>
+        <button name="C" onClick={clear}>C</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button name="/" onClick={handleClick}>/</button>
+        <button name="=" onClick={calculate}>=</button>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
